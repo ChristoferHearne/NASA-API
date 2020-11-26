25113d05c32cac500494c2db9de4da2c0bdcec67
Christofer Hearne <chhe1903@student.miun.se>
2020-11-26 13:42:32 +0100

    Google Map created

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ searchButton.addEventListener("click", ()=>{
   })
 async function sendAPIRequest(){
     let API_KEY = "fdSFchz2wIqT1ZnJuhklfhIAyYRHCj3MKz88E5uR" 
-    let response = await fetch(`https://api.nasa.gov/planetary/earth/assets?lon=${long}&lat=${lat}&&dim=1&api_key=${API_KEY}`);
+    let response = await fetch(`https://api.nasa.gov/planetary/earth/assets?lon=${long}&lat=${lat}&date=2019-10-11&&dim=0.5&api_key=${API_KEY}`);
     let data = await response.json();
     console.log(data); 
     useAPIdata(data); 
 }
 
 function useAPIdata(data){
-     document.querySelector("#content").innerHTML += `<img src="${data.url}">`; 
+     //document.querySelector("#content").innerHTML += `<img src="${data.url}">`; 
 }
+
+// Google Maps API KEY: AIzaSyC7bJrPuUJsdXZutdVouq0GQmk10SFdVLk
