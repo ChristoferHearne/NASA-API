63812bd381df33c32239b61214c33b95be37bd84
Christofer Hearne <chhe1903@student.miun.se>
2020-12-09 15:42:03 +0100

    deletions and commenting

--- photo.js
+++ photo.js
@@ -9,22 +9,34 @@ const datepicker = document.getElementById("date_picker");
 
 // EventListeners
 
+// Gets photo of the day on page load
 window.onload = (event) => {
     setTodaysDate();
     sendAPIRequest();  
 }
 
+// Gets media based on dateinput
 photobutton.addEventListener("click", function(event){
+    // Prevent form from submitting 
     event.preventDefault();
+
+    // Add loading animation 
     this.classList.add('is-loading');
+
+    //Clear main content
     clearMainDIV();
+
+    // Send API Request
     sendCustomAPIRequest(datepicker.value); 
 })
 
 //Functions
 async function sendAPIRequest(){
+    // Assign response
     let response = await fetch(`${mediaFeedURL}${API_KEY}`);
+    //Get data
     let data = await response.json();
+    // Error Handling 
     if (!response.ok){
         alert(data.msg);
     }
@@ -35,8 +47,11 @@ async function sendAPIRequest(){
 } 
 
 async function sendCustomAPIRequest(date){
+    // Assign response
     let response = await fetch(`${mediaFeedURL}${API_KEY}&date=${date}`)
+    // Get data 
     let data = await response.json();
+    // Error Handling
     if (!response.ok){
         removeLoadingClass();
         alert(data.msg)
@@ -49,10 +64,12 @@ async function sendCustomAPIRequest(date){
 
 
 function useAPIData(data){
+    // Format youtube.watch link from data 
     const youtubelink = "https://www.youtube.com/watch?v="+data.url.slice(30,41);
+
+    // If photo of the day is an image 
     if (data.media_type === "image")
     {
-        
         mainele.innerHTML = `
         <img onload="removeLoadingClass()" src=${data.url}>
         <h2>${data.title}</h2>
@@ -60,6 +77,7 @@ function useAPIData(data){
         <p>Date: ${data.date}</p>
         <strong>Copyright: ${data.copyright}</strong>`
     }
+    // If photo of the day is a video
     else if(data.media_type === "video"){
         mainele.innerHTML = `
         <iframe src=${data.url}&autoplay=1 controls height="800" width="1280" onload="removeLoadingClass()" allow="autoplay" allowfullscreen="true"></iframe>
@@ -71,16 +89,19 @@ function useAPIData(data){
     return mainele; 
 }
 
+// Clear content on page 
 function clearMainDIV(){
     mainele.innerHTML = ""; 
 }
 
+// Sets todays date in the date-input 
 function setTodaysDate(){
     var today = new Date(); 
     var date = new Date(today.getFullYear(), today.getMonth(), today.getDate()+1).toISOString().slice(0,10);
     datepicker.value = date; 
 }
 
+// Stop loading animation 
 function removeLoadingClass(){
     photobutton.classList.remove('is-loading'); 
 }
--- satellite.js
+++ satellite.js
@@ -1,58 +1,76 @@
-// NASA API 
+// Variables  
 let lat;
 let long; 
 var previousMarker;
 let API_KEY = "fdSFchz2wIqT1ZnJuhklfhIAyYRHCj3MKz88E5uR" 
 
+// Selectors
 let satelliteButton = document.querySelector(".satellite_button");
 
-window.onload = (event) => {
-  setTodaysDate(); 
-}
+// EventListeners
 
+// Get satellite image on click
 satelliteButton.addEventListener("click", function (event){
+    // Prevent form from submitting
     event.preventDefault();
+
+    // Add loading animation
     this.classList.add('is-loading');
+
+    // Clear img-section 
     clearIMG(); 
+
+    // Get API data
     sendAPIRequest();
   })
 
 async function sendAPIRequest(){
+    // Assign response
     let response = await fetch(`https://api.nasa.gov/planetary/earth/assets?lon=${long}&lat=${lat}&date=2019-10-11&&dim=0.5&api_key=${API_KEY}`);
+
+    // Get data
     let data = await response.json();
+
+    // Error Handling
     if (!response.ok){
       removeLoadingClass(); 
       alert(data.msg);
-  }
-  else{
+    }
+    else{
       useAPIdata(data); 
-  }
+    }
 }
 
+// Clear main img-section 
 function clearIMG(){
   document.querySelector(".satellite_img").src = ""; 
 }
 
-// Get satellite Image from NASA API
+// Load satellite image into page 
 function useAPIdata(data){
      document.querySelector(".satellite_img").src = data.url;
 }
 
 // Google Maps
+
+  // Assign map-variable
   let map;
   
+  // Create Google Map
   function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
   center: { lat: 60.00418, lng: 15.79316 },
   zoom: 8,
   });
-  
+
+  // Place new location marker on click
   google.maps.event.addListener(map, 'click', function(event) {
     if (previousMarker)
         previousMarker.setMap(null);
     placeMarker(event.latLng);
  });
- 
+
+ // Get position of marker and place it
  function placeMarker(location) {
      previousMarker = new google.maps.Marker({
          position: location, 
@@ -65,6 +83,7 @@ function useAPIdata(data){
 
 }
 
+// Stop loading animation
 function removeLoadingClass(){
   satelliteButton.classList.remove('is-loading'); 
 }
